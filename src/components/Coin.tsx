7705f29f0777dd818240f37ceb3df922b6de28ce
grantgariepy <grantgariepy@protonmail.com>
2022-08-15 14:33:49 -0600

    reformat coin

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { createTheme,  styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -49,140 +54,31 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap}) => 
   // };
     return (
        <>
-       <Box sx={{
-          flexGrow: 2,
-          bgcolor: 'background.paper',
-          boxShadow: 1,
-          borderRadius: 2,
-          p: 2,
-          minWidth: 300,
-          mb: 3,
-        }}>
-          <Grid container spacing={1} columns={12}  mt={1}>
-            <Grid 
-              item xs={4}
-              display="flex"
-              direction="row" 
-              alignItems="center"
-              justifyContent="start">
-
-                <Typography ml={3} mr={2}>
-                  <img src={image} width={40} height={40}/>
-                </Typography>
-                <Typography 
-                  textAlign="left"
-                  variant="h5" 
-                  component="div">
-                    {name}
-                </Typography>
-                <Typography 
-                  variant="subtitle1" 
-                  component="div"
-                  textTransform="uppercase"
-                  ml={1}>
-                    {symbol}
-                </Typography>
-              </Grid>   
-              
-             <Grid 
-          item xs={8} 
-          display="flex"
-          direction="row" 
-          alignItems="center"
-          justifyContent="space-between">
-                  <Typography 
-                    variant="body2" 
-                    color="text.secondary">
-                      ${price.toLocaleString()}
-                  </Typography>
-                    {priceChange < 0 ? (
-                    <Typography 
-                    variant="body2" 
-                    color="red">
-                        {priceChange.toFixed()}%
-                  </Typography>
-                    ) : (
-                      <Typography 
-                      variant="body2"
-                      color="green">
-                        {priceChange.toFixed()}%
-                  </Typography>
-                  )}
-                  <Typography 
-                    variant="body2"
-                    color="text.secondary">
-                    <b> Volume:</b> ${volume.toLocaleString()}
-                  </Typography>
-                  <Typography 
-                    variant="body2" 
-                    color="text.secondary">
-                    <b> Market Cap:</b> ${marketcap.toLocaleString()}
-                  </Typography>
+        <Card >
+          <CardActionArea sx={{display:"flex", direction:"row", justifyContent:"center"}}>
             
-                  </Grid>
-          </Grid>
-        </Box>
-            {/* <Grid container spacing={1} justifyContent="center" >
-              {[darkTheme].map((theme, index) => (
-                <Grid item xs={12} key={index} justifyContent="center">
-                  <ThemeProvider theme={theme} >
-                    <Box
-                      sx={{
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        p: 1,
-                        display: 'row',
-                        gridTemplateColumns: { md: '1fr 1fr' },
-                        gap: 1,
-                      }}
-                    >
-                      {[6].map((elevation) => (
-                        <Item key={elevation} elevation={elevation} >
-                          <Typography gutterBottom variant="h5" component="div" >
-                            {name}
-                          </Typography>
-                          <Typography gutterBottom variant="subtitle1" component="div">
-                            {symbol}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            ${price.toLocaleString()}
-                          </Typography>
-                          {priceChange < 0 ? (
-                            <Typography variant="body2" color="red">
-                              {priceChange.toFixed()}%
-                          </Typography>
-                          ) : (
-                            <Typography variant="body2" color="green">
-                              {priceChange.toFixed()}%
-                          </Typography>
-                          )}
-                          <Typography variant="body2" color="text.secondary">
-                            <b> Volume:</b> ${volume.toLocaleString()}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            <b> Market Cap:</b> ${marketcap.toLocaleString()}
-                          </Typography>
-                        </Item>
-                      ))}
-                    </Box>
-                  </ThemeProvider>
-                </Grid>
-              ))}
-            </Grid> */}
+            <CardContent  sx={{display:"flex", justifyContent:"space-between"}}>
+              <img src={image} alt="coin" width="40px" height="40px"/>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+              <Typography>
+                {symbol}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              ${price.toLocaleString()}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        
             
             
             
-            {/* <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-              >
-              <p>hi</p>
-            </ExpandMore> */}
+            
           
         
-        {/* <Chart /> */}
+        
               </>
       
         // <div className="coin-container dark:bg-gray-900">
